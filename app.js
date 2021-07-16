eb4837ed25169c34b56f8707d860df110a19982a
santiagoBassi <santiagobassi21@gmail.com>
2021-07-16 12:21:05 -0300

    termine el recover password, login y register

--- app.js
+++ app.js
@@ -26,3 +26,7 @@ app.get('/login', (req, res) => {
 app.get('/register', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/register.html'))
 });
+
+app.get('/recoverpassword', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/recover-password.html'))
+})
