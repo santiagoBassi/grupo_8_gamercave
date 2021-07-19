aa80e5b408d34478248643183ec862ee40d16e25
Carmo1922 <agustincarmona8@gmail.com>
2021-07-19 19:30:58 -0300

    subi los html y css de producto y carrito junto con un js para carrito

---
+++ public/js/carrito.js
@@ -0,0 +1,29 @@
+
+/*Botones de agregar o disminuir cantidad*/
+function increaseValue() {
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number').value = value;
+  }
+  
+  function decreaseValue() {
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value < 1 ? value = 1 : '';
+    value--;
+    document.getElementById('number').value = value;
+  }
+
+/*Boton de eliminar producto*/
+
+ var cont = 0;
+ 
+    function borrar() {
+        var borrardiv = document.getElementById("container").lastChild;
+        document.getElementById("container").removeChild(borrardiv);
+        cont--;
+
+    }
+
+    
