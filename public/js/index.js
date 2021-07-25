6d26f3cca6613de4b141fc61af9783c60e0adc2c
santiagoBassi <santiagobassi21@gmail.com>
2021-07-25 19:00:49 -0300

    actualice home

--- public/js/index.js
+++ public/js/index.js
@@ -28,6 +28,11 @@ function plusSlides(n) {
     showSlides(slideIndex += n);
 }
 
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
 function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("mySlides");
@@ -41,7 +46,20 @@ function showSlides(n) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
     slides[slideIndex - 1].style.display = "block";
-
+    dots[slideIndex - 1].className += " active";
 }
+var slideIndex = 0;
+showSlides();
 
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 8000); // Change image every 2 seconds
+}
 /*termina slider*/
