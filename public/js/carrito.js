ac4cc5cc8b643a7d27343f49fb41cb236181f3e0
santiagoBassi <santiagobassi21@gmail.com>
2021-07-25 21:34:53 -0300

    header en product detail

--- public/js/carrito.js
+++ public/js/carrito.js
@@ -1,29 +1,31 @@
-
+/*comienza header*/
+function showmenu() {
+    document.getElementById("otro-modo").classList.toggle("show");
+}
+/*termina header*/
 /*Botones de agregar o disminuir cantidad*/
 function increaseValue() {
     var value = parseInt(document.getElementById('number').value, 10);
     value = isNaN(value) ? 0 : value;
     value++;
     document.getElementById('number').value = value;
-  }
-  
-  function decreaseValue() {
+}
+
+function decreaseValue() {
     var value = parseInt(document.getElementById('number').value, 10);
     value = isNaN(value) ? 0 : value;
     value < 1 ? value = 1 : '';
     value--;
     document.getElementById('number').value = value;
-  }
+}
 
 /*Boton de eliminar producto*/
 
- var cont = 0;
- 
-    function borrar() {
-        var borrardiv = document.getElementById("container").lastChild;
-        document.getElementById("container").removeChild(borrardiv);
-        cont--;
+var cont = 0;
 
-    }
+function borrar() {
+    var borrardiv = document.getElementById("container").lastChild;
+    document.getElementById("container").removeChild(borrardiv);
+    cont--;
 
-    
+}
