60187ecdcc4a30b274e97e609766b4b59dfe1423
santiagoBassi <santiagobassi21@gmail.com>
2021-08-21 20:04:37 -0300

    eliminar imagenes obsoletas funcionando

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -123,9 +123,9 @@ const controlador = {
                     updatedProduct['img1'] = req.files[0].filename;
                     updatedProduct['img2'] = req.files[1].filename;
                     updatedProduct['img3'] = req.files[2].filename;
-                    fs.unlinkSync('../public/images/products/' + product.img1);
-                    fs.unlinkSync('../public/images/products/' + product.img2);
-                    fs.unlinkSync('../public/images/products/' + product.img3);
+                    fs.unlinkSync(`./public/images/products/${product.img1}`);
+                    fs.unlinkSync(`./public/images/products/${product.img2}`);
+                    fs.unlinkSync(`./public/images/products/${product.img3}`);
                 };
 
                 function caracteristicas(cantidadCaracteristicas) {
@@ -165,6 +165,14 @@ const controlador = {
         let updatedProducts = products.filter(product => {
             return product.id != req.params.id;
         });
+
+        products.forEach(product => {
+            if (product.id == req.params.id) {
+                fs.unlinkSync(`./public/images/products/${product.img1}`);
+                fs.unlinkSync(`./public/images/products/${product.img2}`);
+                fs.unlinkSync(`./public/images/products/${product.img3}`);
+            }
+        })
 
         let updatedProductsJSON = JSON.stringify(updatedProducts);
 
