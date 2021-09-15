9588db20785d4b99bf6dcb2469fb566f66af5468
santiagoBassi <santiagobassi21@gmail.com>
2021-09-15 11:24:30 -0300

    agregar y eliminar caracteristicas de productos en productCreate

--- public/js/productCrate.js
+++ public/js/productCrate.js
@@ -1,3 +1,19 @@
+function uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16 | 0,
+            v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
+
+function borrarCampo(carac, value) {
+    const caracABorrar = document.getElementById(carac);
+    const valueABorrar = document.getElementById(value)
+
+    caracABorrar.remove();
+    valueABorrar.remove();
+}
+
 function agregarCampos() {
 
     const lugarParaInsertatCarac = document.getElementById('carac');
@@ -5,20 +21,40 @@ function agregarCampos() {
 
     const inputCarac = document.createElement('input');
     const inputValue = document.createElement('input');
+    const divContainerDeleteField = document.createElement('div');
+    const icono = document.createElement('i');
 
     inputCarac.className = 'input';
     inputValue.className = 'input';
+    divContainerDeleteField.className = 'containerDeleteField';
+    icono.classList = "deleteField fas fa-times";
+
 
     inputCarac.name = 'characteristic';
     inputValue.name = 'value';
 
+    inputCarac.placeholder = 'Ej: color';
+    inputValue.placeholder = 'Ej: rojo';
 
-    lugarParaInsertatCarac.appendChild(inputCarac);
+    let idCarac = uuidv4();
+    let idValue = uuidv4();
+
+    icono.onclick = function() {
+        borrarCampo(idCarac, idValue)
+    }
+
+    divContainerDeleteField.setAttribute('id', idCarac);
+    inputValue.setAttribute('id', idValue);
+
+
+    divContainerDeleteField.appendChild(icono);
+    divContainerDeleteField.appendChild(inputCarac);
+
+    lugarParaInsertatCarac.appendChild(divContainerDeleteField);
     lugarParaInsertatValue.appendChild(inputValue);
 
 
-    let elementosTotales = lugarParaInsertatCarac.getElementsByTagName('input').length;
+    let elementosTotales = lugarParaInsertatValue.getElementsByTagName('input').length;
     cantidadElementos.value = elementosTotales;
-
 
 }
