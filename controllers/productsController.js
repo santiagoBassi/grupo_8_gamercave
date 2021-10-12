23f6f1091db9d659de0fb727d3de1c01c418377b
santiagoBassi <santiagobassi21@gmail.com>
2021-10-12 20:51:20 -0300

    seccion products con base de datos

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,17 +1,16 @@
-const fs = require('fs');
-
-
 const db = require('../database/models/index.js');
-
 const Product = db.Product;
 
 
 const controlador = {
-
     products: (req, res) => {
-        let productsJSON = fs.readFileSync('data/products.json', { encoding: 'utf-8' });
-        let products = JSON.parse(productsJSON);
-        return res.render('./products/products', { products: products });
+        Product.findAll({
+                include: ['category', 'characteristics', 'images']
+            })
+            .then(products => {
+                return res.render('./products/products', { products });
+            })
+
     },
     productDetail: (req, res) => {
         Product.findByPk(req.params.id, {
@@ -25,28 +24,10 @@ const controlador = {
                 }
                 return res.render('./products/productDetail', { product, images });
             })
-            /*
-        let productosJSON = fs.readFileSync('data/products.json', { encoding: 'utf-8' });
-        let productos = JSON.parse(productosJSON);
 
-        productos.forEach(producto => {
-            if (producto.id == req.params.id) {
-                let claves = Object.keys(producto);
-
-                let caracteristicas = {};
-                for (let i = 8; i < claves.length; i++) {
-                    caracteristicas[claves[i]] = producto[claves[i]];
-                }
-
-                return res.render('./products/productDetail', { caracteristicas: caracteristicas, producto: producto });
-            };
-        });
-    */
     },
     productCart: (req, res) => {
         return res.render('./products/productCart');
     }
-
-
 };
 module.exports = controlador;
