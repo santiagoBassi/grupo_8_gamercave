b797f9394349da9ee939e88f79045f06f37db746
santiagoBassi <santiagobassi21@gmail.com>
2021-11-13 17:00:06 -0300

    api users detail

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -3,25 +3,39 @@ const User = db.User;
 
 
 const controller = {
-    list: (req,res)=>{
+    list: (req, res) => {
         User.findAll()
-        .then(usersDb => {
-            let users=[];
-            usersDb.forEach(user => {
-                let userForApi ={
+            .then(usersDb => {
+                let users = [];
+                usersDb.forEach(user => {
+                    let userForApi = {
+                        id: user.id,
+                        name: user.name,
+                        email: user.email,
+                        detail: `/api/users/${user.id}`
+                    };
+                    users.push(userForApi);
+                });
+                let response = {
+                    count: usersDb.length,
+                    users: users
+                }
+                res.json(response)
+            })
+    },
+    detail: (req, res) => {
+        User.findByPk(req.params.id)
+            .then(user => {
+                let response = {
                     id: user.id,
-                    name:user.name,
-                    email:user.email,
-                    detail: `/api/users/${user.id}`
-                };
-                users.push(userForApi);
-            });
-            let response = {
-                count: usersDb.length,
-                users: users
-            }
-            res.json(response)
-        })
+                    name: user.name,
+                    lastName: user.lastName,
+                    phone: user.phone,
+                    email: user.email,
+                    image: `/images/users${user.image}`
+                }
+                res.json(response)
+            })
     }
 };
 module.exports = controller;
--- routers/api/users.js
+++ routers/api/users.js
@@ -5,5 +5,7 @@ const apiUsersController = require('../../controllers/api/users.js')
 
 router.get('/', apiUsersController.list);
 
+router.get('/:id', apiUsersController.detail);
+
 
 module.exports = router;
