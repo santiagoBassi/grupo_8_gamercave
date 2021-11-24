cd27e9b950808f5f334bbf4a47af187b8b1fcb52
santiagoBassi <santiagobassi21@gmail.com>
2021-11-23 21:47:50 -0300

    dashboard

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
-import './assets/css/app.css';
+import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from 'react-router-dom';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <React.StrictMode>
     <App />
-  </BrowserRouter>,
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
